a5b23d79a6144159131ec6076edf2ac8aa1bf34d
Jamin Brako <jamin.brako@gmail.com>
2018-12-10 10:48:49 -0500

    initial commit

---
+++ js/problem-set-4-solution.js
@@ -0,0 +1,305 @@
+/*
+ * SOLUTION: Hello.
+ */
+
+function hello() {
+  let result = "Hello, AP Computer Science Principles!"; // variable "result" is defined as an output stating "Hello, AP Computer Science Principles!"
+  document.getElementById("output1").innerHTML = result; // the variable "result" connects to html through id "output1" to function on browser
+
+  check("hello"); //preset code checks if user's solution code is correct or false for "hello" excercise
+}
+
+/*
+ * SOLUTION: Hello, Again.
+ */
+
+function helloAgain() {
+  let name = prompt("What is your name?"); //variable "name" is defined as to create a prompt to ask the user "What is your name?"
+
+  let result = "Hello, " + name + "!"; // variable "result" is defined to say "Hello" [whatever name user inputs]!
+  document.getElementById("output2").innerHTML = result; // the variable "result" connects to html through id "output2" to function on browser
+
+  check("helloAgain", name); //preset code checks if user's solution code is correct or false for "helloAgain" excercise and matches "name" user input
+}
+
+/*
+ * SOLUTION: Celsius.
+ */
+
+function celsius() {
+  let cels = Number(((Math.random() * 1001) - 100).toFixed(2)); //variable "cels" is defined to allow the user to input a numeric value for celsius within the range [-100, 1000], and will be rounded to 2 decimals.
+
+  let fahr = cels * 9 / 5 + 32; // variable "fahr" is defined to include whatever "cels" produces and follows the given formula for converting celsius to fahrenheit
+  let result = cels + " degrees Celsius equals " + fahr.toFixed(2) + " degrees Fahrenheit."; // variable "result" is defined to produce the output of formula for finding "cels", followed by text, " degrees Celsius equals ", than the output for finding "fahr", rounded to 2 decimal places followed by the text " degrees Fahrenheit."
+  document.getElementById("output3").innerHTML = result; // the variable "result" connects to html through id "output3" to function on browser
+
+  check("celsius", cels); //preset code matches "cels" user input and checks if user's solution code is correct or false for "celsius" excercise
+}
+
+/*
+ * SOLUTION: Fahrenheit.
+ */
+
+function fahrenheit() {
+  let fahr = Number(((Math.random() * 1001) - 100).toFixed(2)); // variable "fahr" is defined to allow the user to input a numeric value for fahrenheit within the range [-100, 1000], and will be rounded to 2 decimals.
+
+  let cels = (fahr - 32) * 5 / 9; // variable "cels" is defined to include whatever "cels" produces and follows the given formula for converting fahrenheit to celsius
+  let result = fahr + " degrees Fahrenheit equals " +  cels.toFixed(2) + " degrees Celsius."; // variable "result" is defined to produce the output of formula for finding "fahr", followed by text, " degrees Fahrenheit equals ", than the output for finding "cels", is rounded to 2 decimal places followed by the text " degrees Celsius."
+  document.getElementById("output4").innerHTML = result; // the variable "result" connects to html through id "output4" to function on browser
+
+  check("fahrenheit", fahr); //preset code matches "cels" user input and checks if user's solution code is correct or false for "celsius" excercise and
+}
+
+/*
+ * SOLUTION: Inches.
+ */
+
+function inches() {
+  const MILE = 63360; //variable "MILE" is defined as a constant that stays as the value "63360"
+  const YARD = 36; //variable "YARD" is defined as a constant that stays as the value "36"
+  const FOOT = 12; //variable "FOOT" is defined as a constant that stays as the value "12"
+
+  let input = -1; // variable "input" is defined as the value "-1"
+  while (input < 0) { // while input stays is less than 0, (as defined in variable input above) the code below runs
+    input = Number(prompt("Enter a non-negative integer.")); //variable input is now defined in while loop to produce a prompt that has to be a number to tell the user "Enter a non-negative integer."
+
+    if (input === null) { // if statement states condition that if the input is strictly a null value the code below it should run
+      break; //The break statement terminates the current loop, switch, or label statement and transfers program control to the statement following the terminated statement
+    } else if (Number.isNaN(input)) { //else if statement states condition to specify the new condition to test (if the number is not a number), if the first condition is false regarding the variable "input", the code below it should run
+      input = -1; // variable "input" is defined in "else if" loop to be defined as "-1"
+    } else if (!Number.isInteger(input)) {
+      input = -1; // variable "input" is defined in "else if" loop to be defined as "-1"
+    }
+  }
+
+  if (input !== null) { // if the variable "input" is not a null value (as it checks the value and type), the code below should run
+    let inches = input; // inches is defined in if statement as the input
+    let miles = Math.floor(inches / MILE); //variable "miles" is defined as the the mathematical formula of the input of inches divided by the constant 'MILES', to covert inches to miles
+    inches = inches % MILE; //inches will be represented as a percentage of miles
+    let yards = Math.floor(inches / YARD); // variable "yards" is defined as the the mathematical formula of the input of inches divided by the constant 'YARDS', to covert inches to yards
+    inches = inches % YARD; //inches will be represented as a percentage of yards
+    let feet = Math.floor(inches / FOOT); // variable "feet" is defined as the the mathematical formula of the input of inches divided by the constant 'FOOT', to covert inches to feet
+    inches = inches % FOOT; //inches will be represented as a percentage of feet
+
+    let result = "Miles: " + miles + "<br/>" + // variable "result" will display an output the text "Miles: ", the output of miles, and a line break
+                 "Yards: " + yards + "<br/>" +
+                 "Feet: " + feet + "<br/>" +
+                 "Inches: " + inches;
+    document.getElementById("output5").innerHTML = result;
+  } else {
+    document.getElementById("output5").innerHTML = "";
+  }
+
+  check("inches", input);
+}
+
+/*
+ * SOLUTION: Centimeters.
+ */
+
+function centimeters() {
+  const KILOMETER = 100000;
+  const METER = 100;
+
+  let input = -1; // variable "input" is defined in  loop to be defined as "-1"
+  while (input < 0) {
+    input = Number(prompt("Enter a non-negative integer."));
+
+    if (input === null) {
+      break; //The break statement terminates the current loop, switch, or label statement and transfers program control to the statement following the terminated statement
+    } else if (Number.isNaN(input)) { //else if statement states condition to specify the new condition to test (if the number is not a number), if the first condition is false regarding the variable "input", the code below it should run
+      input = -1; // variable "input" is defined in "else if" loop to be defined as "-1"
+    } else if (!Number.isInteger(input)) {
+      input = -1; // variable "input" is defined in "else if" loop to be defined as "-1"
+    }
+  }
+
+  if (input !== null) {
+    let centimeters = input;
+    let kilometers = Math.floor(centimeters / KILOMETER);
+    centimeters = centimeters % KILOMETER;
+    let meters = Math.floor(centimeters / METER);
+    centimeters = centimeters % METER;
+
+    let result = "Kilometers: " + kilometers + "<br/>" +
+                 "Meters: " + meters + "<br/>" +
+                 "Centimeters: " + centimeters;
+    document.getElementById("output6").innerHTML = result;
+  } else {
+    document.getElementById("output6").innerHTML = "";
+  }
+
+  check("centimeters", input);
+}
+
+/*
+ * SOLUTION: Fluid Ounces.
+ */
+
+function fluidOunces() {
+  const GALLON = 128;
+  const QUART = 32;
+  const PINT = 16;
+  const CUP = 8;
+
+  let input = -1; // variable "input" is defined  as "-1"
+  while (input < 0) {
+    input = Number(prompt("Enter a non-negative integer."));
+
+    if (input === null) {
+      break; //The break statement terminates the current loop, switch, or label statement and transfers program control to the statement following the terminated statement
+    } else if (Number.isNaN(input)) { //else if statement states condition to specify the new condition to test (if the number is not a number), if the first condition is false regarding the variable "input", the code below it should run
+      input = -1; // variable "input" is defined in "else if" loop to be defined as "-1"
+    } else if (!Number.isInteger(input)) {
+      input = -1; // variable "input" is defined in "else if" loop to be defined as "-1"
+    }
+  }
+
+  if (input !== null) {
+    let fluidOunces = input;
+    let gallons = Math.floor(fluidOunces / GALLON);
+    fluidOunces = fluidOunces % GALLON;
+    let quarts = Math.floor(fluidOunces / QUART);
+    fluidOunces = fluidOunces % QUART;
+    let pints = Math.floor(fluidOunces / PINT);
+    fluidOunces = fluidOunces % PINT;
+    let cups = Math.floor(fluidOunces / CUP);
+    fluidOunces = fluidOunces % CUP;
+
+    let result = "Gallons: " + gallons + "<br/>" +
+                 "Quarts: " + quarts + "<br/>" +
+                 "Pints: " + pints + "<br/>" +
+                 "Cups: " + cups + "<br/>" +
+                 "Fluid Ounces: " + fluidOunces;
+    document.getElementById("output7").innerHTML = result;
+  } else {
+    document.getElementById("output7").innerHTML = "";
+  }
+
+  check("fluidOunces", input);
+}
+
+/*
+ * SOLUTION: Ounces.
+ */
+
+function ounces() {
+  const TON = 32000;
+  const POUND = 16;
+
+  let input = -1; // variable "input" is defined as "-1"
+  while (input < 0) {
+    input = Number(prompt("Enter a non-negative integer."));
+
+    if (input === null) {
+      break; //The break statement terminates the current loop, switch, or label statement and transfers program control to the statement following the terminated statement
+    } else if (Number.isNaN(input)) { //else if statement states condition to specify the new condition to test (if the number is not a number), if the first condition is false regarding the variable "input", the code below it should run
+      input = -1; // variable "input" is defined in "else if" loop to be defined as "-1"
+    } else if (!Number.isInteger(input)) {
+      input = -1; // variable "input" is defined in "else if" loop to be defined as "-1"
+    }
+  }
+
+  if (input !== null) {
+    let ounces = input;
+    let tons = Math.floor(ounces / TON);
+    ounces = ounces % TON;
+    let pounds = Math.floor(ounces / POUND);
+    ounces = ounces % POUND;
+
+    let result = "Tons: " + tons + "<br/>" +
+                 "Pounds: " + pounds + "<br/>" +
+                 "Ounces: " + ounces;
+    document.getElementById("output8").innerHTML = result;
+  } else {
+    document.getElementById("output8").innerHTML = "";
+  }
+
+  check("ounces", input);
+}
+
+/*
+ * SOLUTION: Money.
+ */
+
+function money() {
+  const DOLLAR = 100;
+  const QUARTER = 25;
+  const DIME = 10;
+  const NICKEL = 5;
+
+  let input = -1; // variable "input" is defined as "-1"
+  while (input < 0) {
+    input = Number(prompt("Enter a non-negative integer."));
+
+    if (input === null) {
+      break; //The break statement terminates the current loop, switch, or label statement and transfers program control to the statement following the terminated statement
+    } else if (Number.isNaN(input)) { //else if statement states condition to specify the new condition to test (if the number is not a number), if the first condition is false regarding the variable "input", the code below it should run
+      input = -1; // variable "input" is defined in "else if" loop to be defined as "-1"
+    } else if (!Number.isInteger(input)) {
+      input = -1; // variable "input" is defined in "else if" loop to be defined as "-1"
+    }
+  }
+
+  if (input !== null) {
+    let pennies = input;
+    let dollars = Math.floor(pennies / DOLLAR);
+    pennies = pennies % DOLLAR;
+    let quarters = Math.floor(pennies / QUARTER);
+    pennies = pennies % QUARTER;
+    let dimes = Math.floor(pennies / DIME);
+    pennies = pennies % DIME;
+    let nickels = Math.floor(pennies / NICKEL);
+    pennies = pennies % NICKEL;
+
+    let result = "Dollars: " + dollars + "<br/>" +
+                 "Quarters: " + quarters + "<br/>" +
+                 "Dimes: " + dimes + "<br/>" +
+                 "Nickels: " + nickels + "<br/>" +
+                 "Pennies: " + pennies;
+    document.getElementById("output9").innerHTML = result;
+  } else {
+    document.getElementById("output9").innerHTML = "";
+  }
+
+  check("money", input);
+}
+
+/*
+ * SOLUTION: Change.
+ */
+
+function change() {
+  const QUARTER = 25;
+  const DIME = 10;
+  const NICKEL = 5;
+
+  let input = -1; // variable "input" is defined as "-1"
+  while (input < 0 || input >= 1) {
+    input = Number(prompt("Enter a non-negative number less than 1.00."));
+
+    if (input === null) {
+      break; //The break statement terminates the current loop, switch, or label statement and transfers program control to the statement following the terminated statement
+    } else if (Number.isNaN(input)) { //else if statement states condition to specify the new condition to test (if the number is not a number), if the first condition is false regarding the variable "input", the code below it should run
+      input = -1; // variable "input" is defined in "else if" loop to be defined as "-1"
+    }
+  }
+
+  if (input !== null) {
+    let pennies = input;
+    pennies = Number((pennies * 100.00).toFixed(0));
+    let quarters = Math.floor(pennies / QUARTER);
+    pennies = pennies % QUARTER;
+    let dimes = Math.floor(pennies / DIME);
+    pennies = pennies % DIME;
+    let nickels = Math.floor(pennies / NICKEL);
+    pennies = pennies % NICKEL;
+
+    let coins = quarters + dimes + nickels + pennies;
+    document.getElementById("output10").innerHTML = coins + (coins === 1 ? " coin." : " coins.");
+  } else {
+    document.getElementById("output10").innerHTML = "";
+  }
+
+  check("change", input);
+}
---
+++ js/problem-set-4.js
@@ -0,0 +1,233 @@
+/*
+ * Hello. 2 points.
+ */
+
+function hello() {
+  var HelloText = document.getElementById("output1");
+  HelloText.innerHTML = "Hello, AP Computer Science Principles!";
+  ////////////////// DO NOT MODIFY
+  check("hello"); // DO NOT MODIFY
+  ////////////////// DO NOT MODIFY
+}
+
+/*
+ * Hello, Again. 3 points.
+ */
+
+function helloAgain() {
+
+  //////////// DO NOT MODIFY      Use the @name variable to store
+  let name; // DO NOT MODIFY      the value that the user enters
+  //////////// DO NOT MODIFY      in response to your prompt.
+
+  name = prompt("Please enter your name.");
+    document.getElementById("output2").innerHTML = ("Hello, " + name + "!");
+  ///////////////////////////// DO NOT MODIFY
+  check("helloAgain", name); // DO NOT MODIFY
+  ///////////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Celsius. 5 points.
+ */
+
+function celsius() {
+
+  //////////////////////////////////////////////////////// DO NOT MODIFY
+  let cels = ((Math.random() * 1001) - 100).toFixed(2); // DO NOT MODIFY
+  //////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // The above code generates a random number between -100 and 1000
+  // (inclusive), and rounds this value to 2 decimal places.
+  document.getElementById("output3").innerHTML = cels + " degrees Celsius equals " + ((cels * 1.8) + 32).toFixed(2) + " degrees Fahrenheit.";
+  ////////////////////////// DO NOT MODIFY
+  check("celsius", cels); // DO NOT MODIFY
+  ////////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Fahrenheit. 5 points.
+ */
+
+function fahrenheit() {
+
+  //////////////////////////////////////////////////////// DO NOT MODIFY
+  let fahr = ((Math.random() * 1001) - 100).toFixed(2); // DO NOT MODIFY
+  //////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // The above code generates a random number between -100 and 1000
+  // (inclusive), and rounds this value to 2 decimal places.
+
+  document.getElementById("output4").innerHTML = fahr + " degrees Fahrenheit equals " + ((fahr - 32) * 5/9).toFixed(2) + " degrees Celsius.";
+
+  ///////////////////////////// DO NOT MODIFY
+  check("fahrenheit", fahr); // DO NOT MODIFY
+  ///////////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Inches. 5 points.
+ */
+
+function inches() {
+
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+  let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // You are free to modify the value of inches, which you'll likely
+  // need to do. Please do not modify the value of input.
+
+  ////////////////////// DO NOT MODIFY
+  let inches = input; // DO NOT MODIFY
+  ////////////////////// DO NOT MODIFY
+
+  let miles=Math.floor(inches/63360);
+  let yards=Math.floor((inches - (miles * 63360))/36);
+  let feet=Math.floor((inches - (miles * 63360) - (yards*36))/12);
+  inches=Math.floor((inches - (miles * 63360) - (yards*36) - (feet*12)));
+document.getElementById("output5").innerHTML = "Miles: " + miles +"<br/>Yards: " + yards + "<br/>Feet: " + feet +"<br/>Inches: " + inches;
+  ////////////////////////// DO NOT MODIFY
+  check("inches", input); // DO NOT MODIFY
+  ////////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Centimeters. 5 points.
+ */
+
+function centimeters() {
+
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+  let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // You are free to modify the value of centimeters, which you'll
+  // likely need to do. Please do not modify the value of input.
+
+  /////////////////////////// DO NOT MODIFY
+  let centimeters = input; // DO NOT MODIFY
+  /////////////////////////// DO NOT MODIFY
+
+  let kilometers = Math.floor(centimeters/100000);
+  let meters = Math.floor((centimeters) - (kilometers*100000)/100);
+  centimeters = Math.floor(centimeters - (kilometers*100000) - (meters*100));
+  document.getElementById("output6").innerHTML =  "Kilometers: " + kilometers + "<br/>Meters: " + meters + "<br/>Centimeters: " + centimeters
+  /////////////////////////////// DO NOT MODIFY
+  check("centimeters", input); // DO NOT MODIFY
+  /////////////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Fluid Ounces. 5 points.
+ */
+
+function fluidOunces() {
+
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+  let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // You are free to modify the value of fluidOunces, which you'll
+  // likely need to do. Please do not modify the value of input.
+
+  /////////////////////////// DO NOT MODIFY
+  let fluidOunces = input; // DO NOT MODIFY
+  /////////////////////////// DO NOT MODIFY
+
+  let gallons = Math.floor(fluidOunces/128);
+  let quarts = Math.floor(fluidOunces - ((gallons*128)/32));
+  let pints = Math.floor(fluidOunces - (gallons*128) - ((quarts*32)/16));
+  let cups = Math.floor(fluidOunces - (gallons*128) - (quarts*32) - ((pints*16)/8));
+  fluidOunces = Math.floor(fluidOunces - (gallons*128) - (quarts*32) - (pints*16) - (cups*8));
+  document.getElementById("output7").innerHTML = "Gallons: " + gallons + "<br/>Quarts: " + quarts + "<br/>Pints: " + pints
+    + "<br/>Cups: " + cups + "<br/>Fluid Ounces: " + fluidOunces;
+  /////////////////////////////// DO NOT MODIFY
+  check("fluidOunces", input); // DO NOT MODIFY
+  /////////////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Ounces. 5 points.
+ */
+
+function ounces() {
+
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+  let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // You are free to modify the value of ounces, which you'll likely
+  // need to do. Please do not modify the value of input.
+
+  ////////////////////// DO NOT MODIFY
+  let ounces = input; // DO NOT MODIFY
+  ////////////////////// DO NOT MODIFY
+
+  let tons=Math.floor(ounces/32000);
+  let pounds=Math.floor((ounces - (tons*32000))/16);
+  ounces=Math.floor(ounces - (tons*32000) - (pounds*16));
+  document.getElementById("output8").innerHTML = "Tons: " + tons + "<br/>Pounds: " + pounds + "<br/>Ounces: " + ounces;
+
+  ////////////////////////// DO NOT MODIFY
+  check("ounces", input); // DO NOT MODIFY
+  ////////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Money. 7 points.
+ */
+
+function money() {
+
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+  let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // You are free to modify the value of pennies, which you'll likely
+  // need to do. Please do not modify the value of input.
+
+  /////////////////////// DO NOT MODIFY
+  let pennies = input; // DO NOT MODIFY
+  /////////////////////// DO NOT MODIFY
+
+  let dollars=Math.floor((pennies)/100);
+  let quarters=Math.floor((pennies - (dollars*100))/25);
+  let dimes=Math.floor((pennies - (dollars*100) - (quarters*25))/10);
+  let nickels=Math.floor((pennies - (dollars*100) - (quarters*25) - (dimes*10))/5);
+  pennies = Math.floor(pennies - (dollars*100) - (quarters*25) - (dimes*10) - (nickels*5));
+  document.getElementById("output9").innerHTML = ("Dollars: " + dollars + "<br/>Quarters: " + quarters + "<br/>Dimes: " + dimes + "<br/>Nickels: " + nickels + "<br/>Pennies: " + pennies);
+
+
+  ///////////////////////// DO NOT MODIFY
+  check("money", input); // DO NOT MODIFY
+  ///////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Change. 8 points.
+ */
+
+function change() {
+
+  ///////////////////////////////////////////////////////////////////// DO NOT MODIFY
+  let input = prompt("Enter a non-negative number less than 1.00."); // DO NOT MODIFY
+  ///////////////////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // You are free to modify the value of amount, which you'll likely
+  // need to do. Please do not modify the value of input.
+
+  ////////////////////// DO NOT MODIFY
+  let amount = input; // DO NOT MODIFY
+  ////////////////////// DO NOT MODIFY
+
+  amount*=100
+   let quarters = Math.floor((amount)/25);
+   let dimes = Math.floor((amount - (quarters*25))/10);
+   let nickels = Math.floor((amount - (quarters*25) - (dimes*10))/5);
+   let pennies = Math.floor(amount - (quarters*25) - (dimes*10) - (nickels*5));
+   document.getElementById("output10").innerHTML = quarters + dimes + nickels + pennies + " coins."
+  ////////////////////////// DO NOT MODIFY
+  check("change", input); // DO NOT MODIFY
+  ////////////////////////// DO NOT MODIFY
+}
